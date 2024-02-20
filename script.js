d35c880a0988ad9a314e5273a83900d37b47ad68
clarks03 <stephen.clark@mail.utoronto.ca>
2024-02-19 19:07:31 -0500

    new website

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function playAudio(url) {
-    new Audio(url).play();
+let animation = document.getElementById('sphealAnimation');
+let count = 0;
+const sensitivity = 70;
+const NUM_FRAMES = 213;
+
+function positiveModulo(a, b) {
+    return ((a % b) + b) % b;
 }
+
+window.addEventListener('wheel', function(event) {
+    count = positiveModulo((count + Math.floor((event.deltaY) / sensitivity)), NUM_FRAMES);
+    animation.seek(count);
+    console.log(count);
+});
